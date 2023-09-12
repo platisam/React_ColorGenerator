98131309eff4e752a526d55722cca901cbff7f57
platisam <platisam@gmail.com>
2023-09-12 17:59:11 +0200

    adding handleSubmit function and setting form

--- src/App.js
+++ src/App.js
@@ -3,10 +3,35 @@ import SingleColor from "./SingleColor";
 
 function App() {
   const [color, setColor] = useState("");
-  const [error, setError] = usestate(false);
+  const [error, setError] = useState(false);
   const [list, setList] = useState([]);
 
-  return <h2>color generator</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello");
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>list goes here</h4>
+      </section>
+    </>
+  );
 }
 
 export default App;
