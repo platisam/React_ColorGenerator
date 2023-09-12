1e5f976392f6c075527d4b93d2bd778799c4bc00
platisam <platisam@gmail.com>
2023-09-12 17:41:02 +0200

    setting states and creating SingleColor component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
+
 function App() {
-  return <div className="App">color generator</div>;
+  const [color, setColor] = useState("");
+  const [error, setError] = usestate(false);
+  const [list, setList] = useState([]);
+
+  return <h2>color generator</h2>;
 }
 
 export default App;
